eb91abd7299e1c7267a94e8664767c5892733b31
Anvesh Mahajan <anveshmahajan777@gmail.com>
2025-05-23 22:15:32 +0530

    latest changes

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
 }))
+console.log("CORS allowed origin:", allowedOrigin);
 app.use(express.json())
 app.use(cookiesParser())
 
